e25f60bfa3d959250ac5f9388935ad0aa1078f7e
metaprosftp <metaprosftp@gmail.com>
2024-06-23 11:35:59 +0700

    Update sftp.py

--- sftp.py
+++ sftp.py
@@ -60,6 +60,7 @@ def normalize_text(text):
 def generate_metadata(model, img):
     caption = model.generate_content([
         "Create a descriptive title in English up to 12 words long, identifying the main elements of the image. "
+        "identify the main elements subject, activity, context, setting, concept , Capture the Essence, Include Relevant Elements, Emphasize Emotion, Contextual Detail, Be Descriptive. "
         "avoid using brand names or copyrighted elements in the title "
         "descriptive title to ensure readability and ease of search ", img
     ])
@@ -67,14 +68,6 @@ def generate_metadata(model, img):
         "Generate up to 40 keywords that are relevant to the image (each keyword must be one word, separated by commas). "
         "avoid using brand names or copyrighted elements in the keywords "
         "Ensure that the keywords are highly suitable for the image, only in English.", img
-    ])
-
-
-    caption = model.generate_content(["Create a descriptive title in English, 10-15 words long, relevant to the stock photo's subject, object, and background. Avoid mentioning human names, brand, or company names, Make sure the keywords are very relevant to the image and provide detailed descriptions.", img])
-    caption = model.generate_content([
-        "Create a descriptive title in English up to 12 words long, identifying the main elements of the image. "
-        "avoid using brand names or copyrighted elements in the title "
-        "descriptive title to ensure readability and ease of search ", img
     ])
 
     # Extracting keywords and ensuring they are single words
