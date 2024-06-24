45234cc23746b0d6f8a5cd36d5509065edfe5741
metaprosftp <metaprosftp@gmail.com>
2024-06-24 23:12:22 +0700

    Update sftp.py

--- sftp.py
+++ sftp.py
@@ -59,7 +59,7 @@ if 'title_prompt' not in st.session_state:
     st.session_state['title_prompt'] = ("Create a descriptive and accurate title in English, based on the provided image, up to 12 words long.")
 
 if 'tags_prompt' not in st.session_state:
-    st.session_state['tags_prompt'] = ("Generate at least 40 keywords in order of importance from most relevant to least. The first 10 keywords should have the most weight in search placement. Ensure each keyword is accurate and descriptive, reflecting the subject matter, context, and main elements of the image. Use precise terms to capture unique aspects like location, activity, or theme for specificity. Include relevant demographic characteristics and avoid using brand names, product names, or any third-party intellectual property.")
+    st.session_state['tags_prompt'] = ("Generate 49 keywords in order of importance from most relevant to least. The first 10 keywords should have the most weight in search placement. Ensure each keyword is accurate and descriptive, reflecting the subject matter, context, and main elements of the image. Use precise terms to capture unique aspects like location, activity, or theme for specificity. Include relevant demographic characteristics and avoid using brand names, product names, or any third-party intellectual property.")
 
 # Function to normalize and clean text
 def normalize_text(text):
@@ -80,19 +80,19 @@ def generate_metadata(model, img):
     # Converting keywords to lowercase
     keywords = [word.lower() for word in keywords]
     
-    # Removing duplicates and limiting to 40 words
+    # Removing duplicates and limiting to 49 words
     unique_keywords = list(set(keywords))
     
-    if len(unique_keywords) < 40:
+    if len(unique_keywords) < 49:
         additional_tags = model.generate_content([
-            f"Generate {40 - len(unique_keywords)} additional general and relevant keywords for the provided image.", 
+            f"Generate {49 - len(unique_keywords)} additional general and relevant keywords for the provided image.", 
             img
         ])
         additional_keywords = re.findall(r'\w+', additional_tags.text)
         unique_keywords.extend(additional_keywords)
     
-    # Limiting keywords to 40 words
-    unique_keywords = unique_keywords[:40]
+    # Limiting keywords to 49 words
+    unique_keywords = unique_keywords[:49]
 
     # Joining keywords with commas
     trimmed_tags = ','.join(unique_keywords)
